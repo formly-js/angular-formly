a6d85f76103047b1b11e47ac38c42619f7481516
Grant H <ghelton@gmail.com>
2014-02-06 11:39:24 -0800

    added grunt publish to gh-pages task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,22 @@ module.exports = function(grunt) {
 					keepalive: true
 				},
 			}
+		},
+		'gh-pages': {
+			options: {
+				base: 'src'
+			},
+			src: ['**']
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-gh-pages');
+
+	grunt.registerTask('publish', [
+		'gh-pages'
+	]);
 
 	grunt.registerTask('dev', [
 		'connect:dev'
