c59675d22d319922266f1acf88ecae145152c2c7
Grant H <ghelton@gmail.com>
2014-02-06 14:58:45 -0800

    added pretty printing of JSON

--- src/views/home.js
+++ src/views/home.js
@@ -1,27 +1,21 @@
 'use strict';
-app.controller('home', function($scope) {
+app.controller('home', function($scope, $parse) {
 	// Public Vars
-	$scope.formFields = [
-		{
-			type: 'email'
-		},
-		{
-			type: 'text',
-			label: 'First Name'
-		},
-		{
-			type: 'text',
-			label: 'Last Name'
-		},
-		{
-			type: 'password',
-			label: 'Password'
-		},
-		{
-			type: 'hidden',
-			default: 'secret_code'
-		}
-	];
+	$scope.formFields = [{
+		type: 'email'
+	}, {
+		type: 'text',
+		label: 'First Name'
+	}, {
+		type: 'text',
+		label: 'Last Name'
+	}, {
+		type: 'password',
+		label: 'Password'
+	}, {
+		type: 'hidden',
+		default: 'secret_code'
+	}];
 	$scope.formOptions = {
 		uniqueFormId: '1234',
 		submitCopy: 'Save'
@@ -32,5 +26,19 @@ app.controller('home', function($scope) {
 	// Public Methods
 	$scope.onSubmit = function onSubmit() {
 		$scope.submittedData = $scope.formData;
+	};
+
+	var lastGoodResult;
+	$scope.toPrettyJSON = function(obj, tabWidth) {
+		try {
+			var strippedObj = angular.copy(obj);
+			var result = JSON.stringify(strippedObj, null, Number(tabWidth));
+			lastGoodResult = result;
+
+			return result;
+		} catch (e) {
+			// eat $parse error
+			return lastGoodResult;
+		}
 	};
 });
