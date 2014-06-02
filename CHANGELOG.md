1e633e7442bafdb899bd6bfd734fddf2f98cedac
Kent C. Dodds <kent@doddsfamily.us>
2014-06-02 06:42:47 -0600

    Adding "hiddenExpression" to make it easier to interface with the hide feature

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ Version numbers correspond to `bower.json` version
 
 ## Features
 
-Added `hide` property on fields. Allows devs to conditionally show fields.
+Added `hide` property on fields. Allows devs to conditionally show fields. Also adding `hideExpression` property which is an expression that will be evaluated on the result object (using $parse) to make the api simpler to use.
 
 ## Bug Fixes
 
--- src/directives/formly-form.js
+++ src/directives/formly-form.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('formly.render')
-.directive('formlyForm', function formlyForm() {
+.directive('formlyForm', function formlyForm($parse) {
 	return {
 		restrict: 'AE',
 		templateUrl: 'directives/formly-form.html',
@@ -17,6 +17,16 @@ angular.module('formly.render')
 					//Post gets called after angular has created the FormController
 					//Now pass the FormController back up to the parent scope
 					scope.formOnParentScope = scope[attr.name];
+					angular.forEach(scope.fields, function(field) {
+						if (field.hideExpression) {
+						var getter = $parse(field.hideExpression);
+						scope.$watch(function() {
+							return getter(scope.result);
+						}, function(hide) {
+							field.hide = hide;
+						});
+						}
+					});
 				}
 			}
 		}
