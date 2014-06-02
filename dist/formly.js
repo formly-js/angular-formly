7ce7baabba46a33d5f6d456b9f211bdec5e89ed5
Kent C. Dodds <kent@doddsfamily.us>
2014-06-02 06:43:16 -0600

    grunt build

--- dist/formly.js
+++ dist/formly.js
@@ -80,28 +80,41 @@ angular.module('formly.render').directive('formlyField', [
   }
 ]);
 'use strict';
-angular.module('formly.render').directive('formlyForm', function formlyForm() {
-  return {
-    restrict: 'AE',
-    templateUrl: 'directives/formly-form.html',
-    replace: true,
-    scope: {
-      fields: '=fields',
-      options: '=options',
-      result: '=result',
-      formOnParentScope: '=name'
-    },
-    compile: function (scope, iElement, iAttrs, controller, transcludeFn) {
-      return {
-        post: function (scope, ele, attr, controller) {
-          //Post gets called after angular has created the FormController
-          //Now pass the FormController back up to the parent scope
-          scope.formOnParentScope = scope[attr.name];
-        }
-      };
-    }
-  };
-});
+angular.module('formly.render').directive('formlyForm', [
+  '$parse',
+  function formlyForm($parse) {
+    return {
+      restrict: 'AE',
+      templateUrl: 'directives/formly-form.html',
+      replace: true,
+      scope: {
+        fields: '=fields',
+        options: '=options',
+        result: '=result',
+        formOnParentScope: '=name'
+      },
+      compile: function (scope, iElement, iAttrs, controller, transcludeFn) {
+        return {
+          post: function (scope, ele, attr, controller) {
+            //Post gets called after angular has created the FormController
+            //Now pass the FormController back up to the parent scope
+            scope.formOnParentScope = scope[attr.name];
+            angular.forEach(scope.fields, function (field) {
+              if (field.hideExpression) {
+                var getter = $parse(field.hideExpression);
+                scope.$watch(function () {
+                  return getter(scope.result);
+                }, function (hide) {
+                  field.hide = hide;
+                });
+              }
+            });
+          }
+        };
+      }
+    };
+  }
+]);
 angular.module('formly.render').run([
   '$templateCache',
   function ($templateCache) {
