e7e3e7891d7aff44ddd1bcac1d6e09ee6ace8a89
Kent C. Dodds <kent@doddsfamily.us>
2014-08-21 22:26:49 -0600

    Updates

--- dist/formly.bootstrap.js
+++ dist/formly.bootstrap.js
@@ -2,9 +2,9 @@
 angular.module('formly.render', []);
 // Main Formly Module
 angular.module('formly', ['formly.render']);
-'use strict';
 angular.module('formly.render')
 .provider('formlyConfig', function() {
+	'use strict';
 
 	var templateUrlMap = {};
 	var templateMap = {};
@@ -45,12 +45,12 @@ angular.module('formly.render')
 
 	this.$get = function formlyConfig() {
 		return this;
-	}
+	};
 	
 });
-'use strict';
 angular.module('formly.render')
 .directive('formlyField', ["$http", "$compile", "$templateCache", "formlyConfig", function formlyField($http, $compile, $templateCache, formlyConfig) {
+	'use strict';
 	return {
 		restrict: 'AE',
 		transclude: true,
@@ -73,10 +73,10 @@ angular.module('formly.render')
 					}).then(function(response) {
 						setElementTemplate(response.data);
 					}, function(error) {
-						console.log('Formly Error: Problem loading template for ' + templateUrl, error);
+						console.warn('Formly Error: Problem loading template for ' + templateUrl, error);
 					});
 				} else {
-					console.log('Formly Error: template type \'' + $scope.options.type + '\' not supported.');
+					console.warn('Formly Error: template type \'' + $scope.options.type + '\' not supported.');
 				}
 			}
 			function setElementTemplate(templateData) {
@@ -102,9 +102,9 @@ angular.module('formly.render')
 	};
 }]);
 
-'use strict';
 angular.module('formly.render')
 .directive('formlyForm', function formlyForm() {
+	'use strict';
 	return {
 		restrict: 'E',
 		templateUrl: 'directives/formly-form.html',
@@ -123,7 +123,7 @@ angular.module('formly.render')
 					//Now pass the FormController back up to the parent scope
 					scope.formOnParentScope = scope[attr.name];
 				}
-			}
+			};
 		},
 		controller: ["$scope", "$element", "$parse", function($scope, $element, $parse) {
 			// setup watches for watchExpressions
@@ -138,7 +138,7 @@ angular.module('formly.render')
 							var args = Array.prototype.slice.call(arguments, 0);
 							args.unshift(field);
 							return field.watch.expression.apply(this, args);
-						}
+						};
 					}
 
 					$scope.$watch(watchExpression, function() {
@@ -219,11 +219,68 @@ angular.module('formly.render').run(['$templateCache', function($templateCache) 
   );
 
 }]);
+angular.module('formly.render').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('fields/formly-field-checkbox.html',
+    "<div class=checkbox><label><input type=checkbox aria-describedby={{id}}_description ng-required=options.required ng-disabled=options.disabled ng-model=value> {{options.label || 'Checkbox'}} {{options.required ? '*' : ''}}</label><p id={{id}}_description class=help-block ng-if=options.description>{{options.description}}</p></div>"
+  );
+
+
+  $templateCache.put('fields/formly-field-email.html',
+    "<div class=form-group><label for={{id}}>{{options.label || 'Email'}} {{options.required ? '*' : ''}}</label><input type=email class=form-control id={{id}} placeholder={{options.placeholder}} aria-describedby={{id}}_description ng-required=options.required ng-disabled=options.disabled ng-model=value><p id={{id}}_description class=help-block ng-if=options.description>{{options.description}}</p></div>"
+  );
+
+
+  $templateCache.put('fields/formly-field-hidden.html',
+    "<input type=hidden ng-model=value>"
+  );
+
+
+  $templateCache.put('fields/formly-field-number.html',
+    "<div class=form-group><label for={{id}}>{{options.label || 'Number'}} {{options.required ? '*' : ''}}</label><input type=number class=form-control id={{id}} placeholder={{options.placeholder}} aria-describedby={{id}}_description ng-required=options.required ng-disabled=options.disabled min={{options.min}} max={{options.max}} ng-minlength={{options.minlength}} ng-maxlength={{options.maxlength}} ng-model=value><p id={{id}}_description class=help-block ng-if=options.description>{{options.description}}</p></div>"
+  );
+
+
+  $templateCache.put('fields/formly-field-password.html',
+    "<div class=form-group><label for={{id}}>{{options.label || 'Password'}} {{options.required ? '*' : ''}}</label><input type=password class=form-control id={{id}} aria-describedby={{id}}_description ng-required=options.required ng-disabled=options.disabled ng-trim=\"{{options.trimWhitespace || false}}\" ng-model=value><p id={{id}}_description class=help-block ng-if=options.description>{{options.description}}</p></div>"
+  );
+
+
+  $templateCache.put('fields/formly-field-radio.html',
+    "<div class=radio-group><label class=control-label>{{options.label}} {{options.required ? '*' : ''}}</label><div class=radio ng-repeat=\"(key, option) in options.options\"><label><input type=radio name={{id}} id=\"{{id + '_'+ $index}}\" aria-describedby={{id}}_description ng-value=option.value ng-required=options.required ng-model=$parent.value> {{option.name}}</label><p id={{id}}_description class=help-block ng-if=option.description>{{option.description}}</p></div></div>"
+  );
+
+
+  $templateCache.put('fields/formly-field-select.html',
+    "<div class=form-group><label for={{id}}>{{options.label || 'Select'}} {{options.required ? '*' : ''}}</label><select class=form-control id={{id}} aria-describedby={{id}}_description ng-model=value ng-required=options.required ng-disabled=options.disabled ng-init=\"value = options.default\" ng-options=\"option.name as option.name group by option.group for option in options.options\"></select><p id={{id}}_description class=help-block ng-if=options.description>{{options.description}}</p></div>"
+  );
+
+
+  $templateCache.put('fields/formly-field-text.html',
+    "<div class=form-group><label for={{id}}>{{options.label || 'Text'}} {{options.required ? '*' : ''}}</label><input type=text class=form-control id={{id}} placeholder={{options.placeholder}} aria-describedby={{id}}_description ng-required=options.required ng-disabled=options.disabled ng-model=value><p id={{id}}_description class=help-block ng-if=options.description>{{options.description}}</p></div>"
+  );
+
+
+  $templateCache.put('fields/formly-field-textarea.html',
+    "<div class=form-group><label for={{id}}>{{options.label || 'Text'}} {{options.required ? '*' : ''}}</label><textarea type=text class=form-control id={{id}} rows={{options.lines}} placeholder={{options.placeholder}} aria-describedby={{id}}_description ng-required=options.required ng-disabled=options.disabled ng-model=value>\n" +
+    "\t</textarea><p id={{id}}_description class=help-block ng-if=options.description>{{options.description}}</p></div>"
+  );
+
+}]);
+angular.module('formly.render').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('directives/formly-form.html',
+    "<form class=formly role=form><formly-field ng-repeat=\"field in fields\" options=field form-result=result form-value=result[field.key||$index] form-id=options.uniqueFormId ng-if=!field.hide index=$index></formly-field><div ng-transclude></div></form>"
+  );
+
+}]);
 
 // This file adds the default templates to the formlyTemplateProvider.
 // It is excluded from the no-templates build.
-'use strict';
 angular.module('formly.render').config(["formlyConfigProvider", function(formlyConfigProvider) {
+	'use strict';
 	var fields = [
 		'textarea', 'radio', 'select', 'number', 'checkbox',
 		'password', 'hidden', 'email', 'text'
