2255a545c17c6ffd598e13444d10c3e94b2f30fc
Kent C. Dodds <kentcdodds@attask.com>
2014-08-27 09:59:00 -0600

    Updates

--- dist/formly.js
+++ dist/formly.js
@@ -86,9 +86,6 @@ angular.module('formly.render')
 		},
 		controller: ["$scope", function fieldController($scope) {
 			$scope.options = $scope.optionsData();
-			if (typeof $scope.options.default !== 'undefined') {
-				$scope.value = $scope.options.default;
-			}
 			var type = $scope.options.type;
 			if (!type && $scope.options.template) {
 				type = 'template';
@@ -162,14 +159,6 @@ angular.module('formly.render')
 		}]
 	};
 });
-angular.module('formly.render').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('directives/formly-form.html',
-    "<form class=formly role=form><formly-field ng-repeat=\"field in fields\" options=field form-result=result form-value=result[field.key||$index] form-id=options.uniqueFormId ng-if=!field.hide index=$index></formly-field><div ng-transclude></div></form>"
-  );
-
-}]);
 angular.module('formly.render').run(['$templateCache', function($templateCache) {
   'use strict';
 
