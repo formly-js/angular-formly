bdf0ab8bdfffe9bd80432a4e6d58c29765da4b44
Grant Helton <ghelton@gmail.com>
2014-10-17 15:44:57 -0700

    removed old templates, updated build to always be templateless

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,15 @@ var _ = require('lodash-node');
 module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 	require('time-grunt')(grunt);
+
+	var tmp = '.tmp/';
+	var preBuiltDest = tmp + 'build-prep';
+	var builtDest = tmp + 'built';
+	var templatesFile = preBuiltDest + '/formly-templates.js';
+	var targetFilename = 'formly';
+	var concatFile = builtDest + '/' + targetFilename + '.js';
+	var uglifyFile = builtDest + '/' + targetFilename + '.min.js';
+
 	var config = {
 		pkg: grunt.file.readJSON('package.json'),
 		formlyConfig: {
@@ -59,112 +68,82 @@ module.exports = function(grunt) {
 		},
 
 		copy: {
-			deploy: {
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= formlyConfig.base %>/',
+						src: ['**/*.*', '!**/*Spec.js'],
+						dest: preBuiltDest
+					}
+				]
+			},
+			dist: {
 				expand: true,
 				cwd: '.tmp/',
-				src: '**/*-built/**/*.*',
+				src: '**/built/**/*.*',
 				dest: 'dist/',
 				flatten: true,
 				filter: 'isFile',
 			}
 		},
-		ngtemplates: {},
-		concat: {},
-		ngAnnotate: {},
-		uglify: {}
+		concat: {
+			default: {
+				src: [
+					preBuiltDest + '/modules/*.js',
+					preBuiltDest + '/directives/*.js',
+					preBuiltDest + '/providers/*.js',
+					preBuiltDest + '/*.js'],
+				dest: concatFile
+			}
+		},
+		ngtemplates: {
+			default: {
+				options: {
+					module: 'formly.render',
+					append: true,
+					htmlmin: {
+						collapseBooleanAttributes: true,
+						collapseWhitespace: true,
+						removeAttributeQuotes: true,
+						removeComments: true,
+						removeEmptyAttributes: true,
+						removeRedundantAttributes: false,
+						removeScriptTypeAttributes: true,
+						removeStyleLinkTypeAttributes: true
+					}
+				},
+				files: [
+					{
+						cwd: preBuiltDest + '/',
+						src: [ 'fields/**/*.html' ],
+						dest: templatesFile
+					},
+					{
+						cwd: preBuiltDest + '/',
+						src: [ 'directives/**/*.html' ],
+						dest: templatesFile
+					}
+				]
+			}
+		},
+		ngAnnotate: {
+			default: {
+				src: concatFile,
+				dest: concatFile
+			}
+		},
+		uglify: {
+			default: {
+				src: concatFile,
+				dest: uglifyFile,
+				options: {
+					mangle: true,
+					sourceMap: true
+				}
+			}
+		}
 	};
-
-	// targets build config (because they're pretty much identical)
-	var targets = ['vanilla', 'bootstrap', 'no-template'];
-
-	_.each(targets, function(target) {
-		var tmp = '.tmp/' + target;
-		var noTemplates = target === 'no-template';
-
-		var preBuiltDest = tmp + '-build-prep';
-		var builtDest = tmp + '-built';
-
-		var templatesFile = preBuiltDest + '/formly-templates.js';
-		var targetFilename = 'formly.' + target;
-		if (noTemplates) {
-			targetFilename = 'formly';
-		}
-		var concatFile = builtDest + '/' + targetFilename + '.js';
-		var uglifyFile = builtDest + '/' + targetFilename + '.min.js';
-
-		var commonCopyPatterns = ['**/*.*'];
-		if (noTemplates) {
-			commonCopyPatterns.push('!**/formly-config-templates.js');
-		}
-
-		config.copy[target] = {
-			files: [
-				{
-					expand: true,
-					cwd: '<%= formlyConfig.base %>/',
-					src: [target + '/**/*.*', '!**/*Spec.js'],
-					dest: preBuiltDest
-				},
-				{
-					expand: true,
-					cwd: '<%= formlyConfig.base %>/common',
-					src: commonCopyPatterns.concat(['!**/*Spec.js']),
-					dest: preBuiltDest
-				}
-			]
-		};
-
-		var ngtemplatesOptions = {
-			module: 'formly.render',
-			append: true,
-			htmlmin: {
-				collapseBooleanAttributes: true,
-				collapseWhitespace: true,
-				removeAttributeQuotes: true,
-				removeComments: true,
-				removeEmptyAttributes: true,
-				removeRedundantAttributes: false,
-				removeScriptTypeAttributes: true,
-				removeStyleLinkTypeAttributes: true
-			}
-		};
-
-		config.ngtemplates[target] = {
-			options: ngtemplatesOptions,
-			files: [
-				{
-					cwd: preBuiltDest + '/' + target + '/',
-					src: [ 'fields/**/*.html' ],
-					dest: templatesFile
-				},
-				{
-					cwd: preBuiltDest + '/',
-					src: [ 'directives/**/*.html' ],
-					dest: templatesFile
-				}
-			]
-		};
-
-		config.concat[target] = {
-			src: [preBuiltDest + '/modules/**/*.js', preBuiltDest + '/providers/formly-config.js', preBuiltDest + '/**/*.js'],
-			dest: concatFile
-		};
-
-		config.ngAnnotate[target] = {
-			src: concatFile,
-			dest: concatFile
-		};
-
-		config.uglify[target] = {
-			src: concatFile,
-			dest: uglifyFile,
-			options: {
-				mangle: true,
-				sourceMap: true
-			}
-		};
-	});
-
 
 	// Pass config to grunt
 	grunt.initConfig(config);
@@ -174,33 +153,22 @@ module.exports = function(grunt) {
 	]);
 
 	grunt.registerTask('dev', [
+		'build',
 		'connect:dev',
 		'watch'
 	]);
 
-	// the code below creates tasks for build:{{target}} (i.e. build:bootstrap)
-	_.each(targets, function(target) {
-		grunt.registerTask('build:' + target, [
-			'copy:' + target,
-			'ngtemplates:' + target,
-			'concat:' + target,
-			'ngAnnotate:' + target,
-			'uglify:' + target
-		]);
-	});
-
-
-	var buildTasks = _.map(targets, function(target) {
-		return 'build:' + target;
-	});
-	buildTasks = ['jshint', 'mocha'].concat(buildTasks).concat(['clean:dist', 'copy:deploy']);
-
-	grunt.registerTask('build', buildTasks);
-
-	grunt.registerTask('build:onlyBootstrap', [
+	grunt.registerTask('build', [
+		'jshint',
+		'mocha',
 		'clean:tmp',
-		'build:bootstrap',
-		'copy:deploy'
+		'copy:build',
+		'ngtemplates',
+		'concat',
+		'ngAnnotate',
+		'uglify',
+		'clean:dist',
+		'copy:dist'
 	]);
 
 	grunt.registerTask('default', ['build']);
