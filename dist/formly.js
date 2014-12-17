212cf505a9e51cad13082bc7b2966a3d8af0eb8f
Kent C. Dodds <kent+github@doddsfamily.us>
2014-12-17 16:41:02 -0700

    Updates

--- dist/formly.js
+++ dist/formly.js
@@ -104,6 +104,9 @@ angular.module('formly.render')
 			}
 
 			function valueGetterSetter(newVal) {
+				if (!$scope.result || (!$scope.options.key && !$scope.index)) {
+					return;
+				}
 				if (angular.isDefined(newVal)) {
 					$scope.result[$scope.options.key || $scope.index] = newVal;
 				}
@@ -202,7 +205,7 @@ angular.module('formly.render')
 			});
 
 			function setupWatchers(field, index) {
-				if (!field.hasOwnProperty('watch') || !angular.isDefined(watchers)) {
+				if (!field.hasOwnProperty('watch') || !angular.isDefined(field.watch)) {
 					// must check for own property because of this:
 					// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/watch
 					// (－‸ლ)   <-- yes, that's a face palm...
@@ -220,22 +223,24 @@ angular.module('formly.render')
 					var watchExpression = watcher.expression || 'result["' + field.key + '" || ' + index + ']';
 					if (angular.isFunction(watchExpression)) {
 						// wrap the field's watch expression so we can call it with the field as the first arg and the stop function as the last arg as a helper
+						var originalExpression = watchExpression;
 						watchExpression = function formlyWatchExpression() {
 							var args = Array.prototype.slice.call(arguments, 0);
 							args.unshift($scope.fields[index]); // don't just use field here to ensure that we've got the right field reference
 							args.push(stopWatching);
-							return field.watch.expression.apply(this, args);
+							return originalExpression.apply(this, args);
 						};
 						watchExpression.displayName = 'Formly Watch Expression for field for ' + field.key;
 					}
 					var watchListener = watcher.listener;
 					if (angular.isFunction(watchListener)) {
 						// wrap the field's watch listener so we can call it with the field as the first arg and the stop function as the last arg as a helper
+						var originalListener = watchListener;
 						watchListener = function formlyWatchListener() {
 							var args = Array.prototype.slice.call(arguments, 0);
 							args.unshift($scope.fields[index]); // don't just use field here to ensure that we've got the right field reference
 							args.push(stopWatching);
-							return field.watch.listener.apply(this, args);
+							return originalListener.apply(this, args);
 						};
 						watchListener.displayName = 'Formly Watch Listener for field for ' + field.key;
 					}
