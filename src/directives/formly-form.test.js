ece4956d9cafd7a74286efd5416e0068b08fbcde
Koen Weyn <koenweyn@blibo.be>
2015-03-27 10:35:24 +0100

    do not change default formly-form root tag (unit tests added)

--- src/directives/formly-form.test.js
+++ src/directives/formly-form.test.js
@@ -1,8 +1,30 @@
 module.exports = ngModule => {
   describe('formly-form', () => {
+    let $compile, scope;
+
     beforeEach(window.module(ngModule.name));
-    it('should be tested in the future', () => {
-      expect(true).to.be.true;
+    beforeEach(inject((_$compile_, $rootScope) => {
+      $compile = _$compile_;
+      scope = $rootScope.$new();
+    }));
+
+    it('should use ng-form as the default root tag', () => {
+      const el = compileAndDigest('<formly-form form="theForm"></formly-form>');
+      expect(el.length).to.equal(1);
+      expect(el.prop('nodeName').toLowerCase()).to.equal('ng-form');
     });
+
+    it('should use a different root tag when specified', () => {
+      const el = compileAndDigest('<formly-form form="theForm" root-el="form"></formly-form>');
+      expect(el.length).to.equal(1);
+      expect(el.prop('nodeName').toLowerCase()).to.equal('form');
+    });
+
+    function compileAndDigest(template) {
+      const el = $compile(template)(scope);
+      scope.$digest();
+      return el;
+    }
+
   });
 };
