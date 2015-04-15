a52d778385e69b0bdf39d061d014a7d0dcb5b875
Kent C. Dodds <kent+github@doddsfamily.us>
2015-04-15 09:28:41 -0600

    Update examples.js

--- demo/app/states/root/components/data/examples.js
+++ demo/app/states/root/components/data/examples.js
@@ -1,7 +1,7 @@
 /*
  INSTRUCTIONS
  1. Create an example on jsbin by cloning this template: http://jsbin.com/panovu/edit
- 2. Make sure it's awesome, clean, and demonstrates only one thing
+ 2. Make sure it's awesome and clean
  3. Create a PR to this file. Put it in the right category (or make a new one).
  The "jsbinId" is the unique part of the jsbin url ("panovu" is the jsbinId for the template)
  The "slug" is what will appear in the URL for the example. Make it:
