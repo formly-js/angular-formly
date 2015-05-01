cd152f6c2ee717be48ecfd8f0f4a3c223b1c3ff5
Kent C. Dodds <kent+github@doddsfamily.us>
2015-05-01 06:26:04 -0600

    6.2.0

--- dist/formly.js
+++ dist/formly.js
@@ -1,4 +1,4 @@
-// angular-formly version 6.1.0 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
+// angular-formly version 6.2.0 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
 
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -108,7 +108,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	ngModule.constant("formlyApiCheck", formlyApiCheck);
 	ngModule.constant("formlyErrorAndWarningsUrlPrefix", formlyErrorAndWarningsUrlPrefix);
-	ngModule.constant("formlyVersion", ("6.1.0")); // <-- webpack variable
+	ngModule.constant("formlyVersion", ("6.2.0")); // <-- webpack variable
 	
 	ngModule.provider("formlyUsability", formlyUsability);
 	ngModule.provider("formlyConfig", formlyConfig);
@@ -276,7 +276,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 	
-	module.exports = "https://github.com/formly-js/angular-formly/blob/" + ("6.1.0") + "/other/ERRORS_AND_WARNINGS.md#";
+	module.exports = "https://github.com/formly-js/angular-formly/blob/" + ("6.2.0") + "/other/ERRORS_AND_WARNINGS.md#";
 
 /***/ },
 /* 4 */
