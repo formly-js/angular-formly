525e8e675fa85bbe0272ebbc64b6b8f686778576
Kent C. Dodds <kent+github@doddsfamily.us>
2015-05-20 15:40:38 -0600

    6.6.0

--- dist/formly.js
+++ dist/formly.js
@@ -1,4 +1,4 @@
-// angular-formly version 6.5.4 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
+// angular-formly version 6.6.0 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
 
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -108,7 +108,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	ngModule.constant("formlyApiCheck", formlyApiCheck);
 	ngModule.constant("formlyErrorAndWarningsUrlPrefix", formlyErrorAndWarningsUrlPrefix);
-	ngModule.constant("formlyVersion", ("6.5.4")); // <-- webpack variable
+	ngModule.constant("formlyVersion", ("6.6.0")); // <-- webpack variable
 	
 	ngModule.provider("formlyUsability", formlyUsability);
 	ngModule.provider("formlyConfig", formlyConfig);
@@ -193,13 +193,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	  message: formlyExpression.optional
 	}).strict]));
 	
+	var modelChecker = apiCheck.oneOfType([apiCheck.oneOf(["formState"]), apiCheck.object]);
+	
 	var fieldOptionsApiShape = {
 	  $$hashKey: apiCheck.any.optional,
 	  type: apiCheck.shape.ifNot(["template", "templateUrl"], apiCheck.string).optional,
 	  template: apiCheck.shape.ifNot(["type", "templateUrl"], apiCheck.oneOfType([apiCheck.string, apiCheck.func])).optional,
 	  templateUrl: apiCheck.shape.ifNot(["type", "template"], apiCheck.oneOfType([apiCheck.string, apiCheck.func])).optional,
 	  key: apiCheck.oneOfType([apiCheck.string, apiCheck.number]).optional,
-	  model: apiCheck.object.optional,
+	  model: modelChecker.optional,
 	  className: apiCheck.string.optional,
 	  expressionProperties: expressionProperties.optional,
 	  data: apiCheck.object.optional,
@@ -264,7 +266,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  options: formOptionsApi.optional,
 	  hide: apiCheck.bool.optional,
 	  hideExpression: formlyExpression.optional,
-	  model: apiCheck.object.optional,
+	  model: modelChecker.optional,
 	  form: apiCheck.object.optional,
 	  elementAttributes: apiCheck.objectOf(apiCheck.string).optional
 	}).strict;
@@ -301,7 +303,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 	
-	module.exports = "https://github.com/formly-js/angular-formly/blob/" + ("6.5.4") + "/other/ERRORS_AND_WARNINGS.md#";
+	module.exports = "https://github.com/formly-js/angular-formly/blob/" + ("6.6.0") + "/other/ERRORS_AND_WARNINGS.md#";
 
 /***/ },
 /* 4 */
@@ -1518,6 +1520,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      $scope.model = $scope.model || {};
 	      $scope.fields = $scope.fields || [];
 	
+	      angular.forEach($scope.fields, initModel); // initializes the model property if set to 'formState'
 	      angular.forEach($scope.fields, attachKey); // attaches a key based on the index if a key isn't specified
 	      angular.forEach($scope.fields, setupWatchers); // setup watchers for all fields
 	
@@ -1576,6 +1579,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	            field.resetModel();
 	          }
 	        });
+	      }
+	
+	      function initModel(field) {
+	        if (field.model && field.model === "formState") {
+	          field.model = $scope.options.formState;
+	        }
 	      }
 	
 	      function attachKey(field, index) {
