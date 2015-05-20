3ff07f3705326f4e9c24aea3b3031f3b257e6e52
Kent C. Dodds <kent+github@doddsfamily.us>
2015-05-20 09:43:34 -0600

    Fixing version number

--- dist/formly.js
+++ dist/formly.js
@@ -1,4 +1,4 @@
-// angular-formly version 6.5.3 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
+// angular-formly version 6.5.4 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
 
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -108,7 +108,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	ngModule.constant("formlyApiCheck", formlyApiCheck);
 	ngModule.constant("formlyErrorAndWarningsUrlPrefix", formlyErrorAndWarningsUrlPrefix);
-	ngModule.constant("formlyVersion", ("6.5.3")); // <-- webpack variable
+	ngModule.constant("formlyVersion", ("6.5.4")); // <-- webpack variable
 	
 	ngModule.provider("formlyUsability", formlyUsability);
 	ngModule.provider("formlyConfig", formlyConfig);
@@ -301,7 +301,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 	
-	module.exports = "https://github.com/formly-js/angular-formly/blob/" + ("6.5.3") + "/other/ERRORS_AND_WARNINGS.md#";
+	module.exports = "https://github.com/formly-js/angular-formly/blob/" + ("6.5.4") + "/other/ERRORS_AND_WARNINGS.md#";
 
 /***/ },
 /* 4 */
