b07e49c0642894bad920bde350adfeb9bd16084c
Kent C. Dodds <kent+github@doddsfamily.us>
2015-06-30 07:02:27 -0600

    6.16.1

--- dist/formly.js
+++ dist/formly.js
@@ -1,4 +1,4 @@
-//! angular-formly version 6.16.0 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
+//! angular-formly version 6.16.1 built with ♥ by Astrism <astrisms@gmail.com>, Kent C. Dodds <kent@doddsfamily.us> (ó ì_í)=óò=(ì_í ò)
 
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -147,7 +147,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	ngModule.constant('formlyApiCheck', _providersFormlyApiCheck2['default']);
 	ngModule.constant('formlyErrorAndWarningsUrlPrefix', _otherDocsBaseUrl2['default']);
-	ngModule.constant('formlyVersion', ("6.16.0")); // <-- webpack variable
+	ngModule.constant('formlyVersion', ("6.16.1")); // <-- webpack variable
 
 	ngModule.provider('formlyUsability', _providersFormlyUsability2['default']);
 	ngModule.provider('formlyConfig', _providersFormlyConfig2['default']);
@@ -437,7 +437,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	exports["default"] = "https://github.com/formly-js/angular-formly/blob/" + ("6.16.0") + "/other/ERRORS_AND_WARNINGS.md#";
+	exports["default"] = "https://github.com/formly-js/angular-formly/blob/" + ("6.16.1") + "/other/ERRORS_AND_WARNINGS.md#";
 	module.exports = exports["default"];
 
 /***/ },
@@ -1904,7 +1904,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    function updateInitialValue() {
 	      _angularFix2['default'].forEach($scope.fields, function (field) {
-	        if (isFieldGroup(field)) {
+	        if (isFieldGroup(field) && field.options) {
 	          field.options.updateInitialValue();
 	        } else {
 	          field.updateInitialValue();
@@ -1914,9 +1914,9 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    function resetModel() {
 	      _angularFix2['default'].forEach($scope.fields, function (field) {
-	        if (isFieldGroup(field)) {
+	        if (isFieldGroup(field) && field.options) {
 	          field.options.resetModel();
-	        } else {
+	        } else if (field.resetModel) {
 	          field.resetModel();
 	        }
 	      });
