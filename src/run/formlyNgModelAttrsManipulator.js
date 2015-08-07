9663c70b62828f1984015b14c140c48c0945a2f0
Koen Weyn <koenweyn@blibo.be>
2015-08-07 17:15:10 +0200

    Added comment to make sure that the code is deleted when IE8 support is dropped

--- src/run/formlyNgModelAttrsManipulator.js
+++ src/run/formlyNgModelAttrsManipulator.js
@@ -127,6 +127,8 @@ function addFormlyNgModelAttrsManipulator(formlyConfig, $interpolate, formlyWarn
     try {
       return node.querySelectorAll(query);
     } catch (e) {
+      //this code is needed for IE8, as it does not support the CSS3 ':not' selector
+      //it should be removed when IE8 support is dropped
       return getNgModelNodesFallback(node, skip);
     }
   }
