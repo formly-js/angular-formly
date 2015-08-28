e189d6b5fddb060fbb2e0e50dda628051cfc5252
Kent C. Dodds <kent@doddsfamily.us>
2015-08-28 14:39:53 -0600

    fix(build): Add publish:latest 🐳
    Add authorize-push script by @boennemann so travis can push changes which is run as a pre-script,
    then publish:latest runs to checkout the latest branch, commit the built files and move the tag
    (which totally works with the github release, crazy, I know).
    
    #445

---
+++ scripts/authorize-push.js
@@ -0,0 +1,12 @@
+var exec = require('child_process').exec
+
+var GH_TOKEN = process.env.GH_TOKEN
+var repo = require('../package.json').repository.url
+
+if (!(process.env.CI && GH_TOKEN && repo)) {
+  process.exit(1)
+}
+
+exec('git remote set-url origin ' + repo.replace('https://', 'https://' + GH_TOKEN + '@') + ' && ');
+exec('git config user.email formly-js@angular-formly.com && ');
+exec('git config user.name "formly-bot"');
