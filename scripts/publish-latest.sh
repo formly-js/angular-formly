0dcf8a534d09e0c9597f59c1d3fb52bde28d7ca2
Kent C. Dodds <kent@doddsfamily.us>
2015-08-29 07:19:04 -0600

    fix(build): maybe now?

--- scripts/publish-latest.sh
+++ scripts/publish-latest.sh
@@ -8,7 +8,7 @@ git config --global user.email kent+formly-bot@doddsfamily.us
 git config --global user.name formly-bot
 
 echo "setting remote"
-git remote set-url origin https://formly-bot:${BOT_GH_TOKEN}@github.com/formly-js/angular-formly.git
+git remote set-url origin https://formly-bot:$BOT_GH_TOKEN@github.com/formly-js/angular-formly.git
 
 echo "merging master"
 git merge origin/master -m "master merge" -X theirs
