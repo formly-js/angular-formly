a5ea31648a1d91cf24566065f003a53ee760d435
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-30 23:06:40 +0100

    chore: add meteor dependencies

--- package.js
+++ package.js
@@ -1,24 +1,23 @@
-/* global packageFix:true Package:false */
+/* global Package:false */
 // package metadata file for AtmosphereJS
 
 try {
-  packageFix = Package
-
-  packageFix.describe({
+  Package.describe({
     name: 'formly:angular-formly',
     summary: 'angular-formly (official): forms for AngularJS',
     version: '0.0.0-semantically-released.0',
     git: 'https://github.com/formly-js/angular-formly.git',
   })
 
-  packageFix.onUse((api) => {
+  Package.onUse(function(api) {
     api.versionsFrom(['METEOR@1.0'])
+    // api-check
     api.use('wieldo:api-check@7.5.5')
+    api.imply('wieldo:api-check')
+    // angular
+    api.use('angular:angular@1.4.0')
     api.addFiles('dist/formly.js', 'client')
   })
-
 } catch (e) {
-  // little workaround...
-  // ghooks compiles this file and throws ReferenceError because
-  // Package is a global variable available only in Meteor environment
+  //
 }
